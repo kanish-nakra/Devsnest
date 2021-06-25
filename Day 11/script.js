8b0ed6b932c0b0ed54656d107d55193eb92de58d
kanish-nakra <kanish.nakra05@gmail.com>
2021-06-25 14:05:40 +0530

    Updated

---
+++ Day 11/script.js
@@ -0,0 +1,23 @@
+var questions = [
+  {
+    que: "Who is the Batch Leader of Async Team?", 
+    option1:"Welsh",
+    option2:"Shini",
+    option3:"Ashirbad",
+    option4:"Barney",
+    ans:"Welsh",
+
+  },
+  {
+    que: "Who is the under-age child in Async Team?", 
+    option1:"Swag",
+    option2:"Shini",
+    option3:"Barney",
+    option4:"Ayush",
+    ans:"Swag", 
+  },
+]
+
+// const answer = ()
+
+
